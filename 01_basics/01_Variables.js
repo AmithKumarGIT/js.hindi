49722115f480786bbfcfae904d5e284142834954
AmithKumarGIT <46785078+AmithKumarGIT@users.noreply.github.com>
2023-11-04 19:05:39 +0000

    Learned about variable

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountID = 14412
+let accountEmail = "amit77413@gmail.com"
+let accountState;
+var accountPassword = "12345"
+
+/*prefer not to use var due to block scope 
+& functional scope*/
+
+accountcity = "jaipur"
+//accountID = 2 // not allowed
+//console.log(accountID);
+accountEmail = "ammit@gmail.com"
+accountPassword = "1876"
+accountcity = "Bengaluru"
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountcity, accountState])
+
