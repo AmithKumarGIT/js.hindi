77ab447fcfd5224702a8555d16dbee7bfcc2780d
AmithKumarGIT <46785078+AmithKumarGIT@users.noreply.github.com>
2023-11-04 19:22:56 +0000

    Learned data Types

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) - We are using node.js & not browser
+
+let name = "Amit"
+let age = 28
+let isLoggedIn = false
+
+// number => 2 to powe 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);
