7480f2ff49a4a83107183f523f0fd0f98e228cf3
AmithKumarGIT <46785078+AmithKumarGIT@users.noreply.github.com>
2023-11-05 04:33:21 +0000

    Learned Comparision

---
+++ 01_basics/01_comparision.js
@@ -0,0 +1,13 @@
+// avoid comprsion as blow from 3 to 9 line
+
+// console.log("2" > 1)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null < 0)
+
+// === -> strict chcek - it chcek data type as well
+
+console.log("2" === 2)
