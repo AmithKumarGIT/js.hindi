71b8cf98fcccee14a12704878b54ec14a080aee4
AmithKumarGIT <46785078+AmithKumarGIT@users.noreply.github.com>
2023-11-07 17:43:35 +0000

    Learned a bit more about Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,5 +32,53 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Amit"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Amit"))
+// console.log(loginUserMessage())
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(2))
+// Ans -2
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,300,400))
+
+// Ans - [200, 300, 400]
+
+// function calculateCartPrice(val1,val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,300,400,2000))
+
+//ans - [400, 200]
+
+const user = {
+    username: "Amit",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+handleObject({
+    username: "Amit",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400]
+
+function returnSecondvalue(getarray){
+    return getarray[1]
+}
+
+//console.log(returnSecondvalue(myNewArray))
+
+console.log(returnSecondvalue([100, 200, 300]))
